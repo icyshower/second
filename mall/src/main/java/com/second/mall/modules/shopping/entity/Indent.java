8aef52109c31c6b82720ae08cb18adf1daaca743
icyshower <xy1916128213@163.com>
2021-01-04 17:24:40 +0800

    "订单基础的真删改查"

--- mall/src/main/java/com/second/mall/modules/shopping/entity/Indent.java
+++ mall/src/main/java/com/second/mall/modules/shopping/entity/Indent.java
@@ -23,6 +23,6 @@ public class Indent {
     private int addressId;  //收货地址id
     private double indentPrice; //订单总价格
     private LocalDateTime createTime;   //订单创建时间
-    private int state;  //订单状态，0:未付款;1:未收货;2:已收货未评价;3:已评价;4:退货中;5:已退货
+    private int state;  //订单状态，0:未付款;1:未收货;2:已收货未评价;3:已评价;4:退货中;5:已退货;6:订单假删除
 
 }
